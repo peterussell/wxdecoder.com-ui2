40d6681a4101deb4cf35f070e259d34e0a6e6b05
Pete Russell <pinkbatz@gmail.com>
2017-05-13 11:22:29 -0700

    Adds Google Analytics

--- src/components/metar_decoder.js
+++ src/components/metar_decoder.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import ReactGA from 'react-ga';
 
 import SearchBar from './search_bar';
 import MetarList from './metar_list';
 
 export default class MetarDecoder extends Component {
+  constructor() {
+    super();
+
+    // Google Analytics
+    ReactGA.initialize('UA-98122822-1');
+    ReactGA.pageview(window.location.pathname);
+  }
   render() {
     return (
       <div>
