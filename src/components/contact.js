bd8d077ec469322ebe1efbb919a53b9fcd9025e8
Pete Russell <pinkbatz@gmail.com>
2017-06-26 00:06:42 -0700

    Added: Twitter link to Contact page.

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,10 +4,16 @@ export default class Contact extends Component {
   componentDidMount() {
     document.title = "Contact | WX Decoder"
   }
+
   render() {
     return (
       <div>
         <h1 className="wxd-page-header">Contact Information</h1>
+        <p className="contact-text">
+          <img src={require(`../../assets/img/twitter-logo.png`)} />
+          Follow <a href="https://twitter.com/wxdecoder" target="_blank">
+          @wxdecoder</a> on Twitter
+        </p>
       </div>
     );
   }
