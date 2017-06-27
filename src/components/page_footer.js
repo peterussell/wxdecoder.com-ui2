811cc73e66e621ef72a2b6e905a87991e5aa1719
Pete Russell <pinkbatz@gmail.com>
2017-06-26 23:51:48 -0700

    Added: Twitter link to page footer.

--- src/components/page_footer.js
+++ src/components/page_footer.js
@@ -5,7 +5,8 @@ export default class PageFooter extends Component {
     return (
       <div className="wxd-footer">
         <p className="footer-text text-right">
-          &copy; 2017 Peter Russell
+          &copy; 2017 Pete Russell
+           | <a href="https://twitter.com/nz_pete">@nz_pete</a>
         </p>
       </div>
     );
